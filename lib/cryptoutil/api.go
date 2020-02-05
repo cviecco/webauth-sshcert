5d1f5ab6232eb49bbcd34edb0bd634d20b9bb5a6
Camilo Viecco <cviecco@gmail.com>
2020-02-05 10:03:24 -0800

    client using now cryptoutil functions

--- lib/cryptoutil/api.go
+++ lib/cryptoutil/api.go
@@ -1,5 +1,22 @@
 package cryptoutil
 
+import (
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+// Returns 32 bytes of random string encoded
+// with URL base64 encoding
 func GenRandomString() (string, error) {
 	return genRandomString()
+}
+
+// Given the client parameters, generates a valid ssh signature that
+// can be latter used to sing messages back to the server
+func WithAgentGenerateChallengeResponseSignature(clientNonce string,
+	challenge string,
+	agentClient agent.Agent,
+	key *agent.Key) (*ssh.Signature, error) {
+	return withAgentGenerateChallengeResponseSignature(clientNonce,
+		challenge, agentClient, key)
 }
